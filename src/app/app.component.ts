b91a3295d880897f3c645b2b78a969f95f12baf1
simonsimon9 <simonkuang9@outlook.com>
2022-06-08 00:16:29 -0400

    validation form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,48 @@ export interface PlayingCard{
 //A. Decorator
 @Component({
   selector: 'app-root', //C. Service
-  template: `<h1>Hello world! {{title}} <br/>
-  <!-- Show cards in unordered list. -->
-  <ul><li *ngFor="let card of cards" (click)="onSelect(card)">
-  {{card.cardVal}}</li>
-  </ul>
-  <div *ngIf="selectedCard">
-  <h2>{{selectedCard.cardVal}} **</h2>
-  <input [(ngModel)]="selectedCard.cardVal" placeholder="name"/>
-  </div>
-  `
-  
+  template: `
+  <section>
+  <form #myForm="ngForm">
+  Name:
+  <input type="text" pattern="[a-zA-Z ]*" minlength="3" required
+  [(ngModel)]="myName" name="firstName" #firstName="ngModel" >
+  <div [hidden]="firstName.valid || firstName.pristine">
+  This control is invalid.</div>
+  <p *ngIf="firstName?.errors?.['required']">This field is required.</p>
+  <p *ngIf="firstName?.errors?.['pattern']">
+  Only alphabetical characters are allowed.</p>
+  <p *ngIf="firstName?.errors?.['minlength']">
+  This entry must have at least three characters.</p>
+  <button type="submit" class="btn btn-default"
+  [disabled]="!myForm.form.valid" (click)="onSubmit()">Submit</button>
+ 
+ 
+  LastName:
+  <input type="text" pattern="[a-zA-Z ]*" minlength="2" required
+  [(ngModel)]="myLastName" name="LastName" #LastName="ngModel" >
+  <div [hidden]="LastName.valid || LastName.pristine">
+  This control is invalid.</div>
+  <p *ngIf="LastName?.errors?.['required']">This field is required.</p>
+  <p *ngIf="LastName?.errors?.['pattern']">
+  Only alphabetical characters are allowed.</p>
+  <p *ngIf="LastName?.errors?.['minlength']">
+  This entry must have at least three characters.</p>
+  <button type="submit" class="btn btn-default"
+  [disabled]="!myForm.form.valid" (click)="onSubmit()">Submit</button>
+
+</form>
+</section>
+`
 })
 export class AppComponent {
-  title = 'This is Angular!';
-  // Include card data in class.
-  cards:PlayingCard[] = [
-  { cardVal:"Ace", suit:"Spades"},
-  { cardVal:"Two", suit:"Clubs" },
-  { cardVal:"Six", suit:"Hearts"},
-  ];
-  selectedCard!: PlayingCard;
-  onSelect(card: PlayingCard) {
-     alert(card.suit);
-     this.selectedCard = card;
+  myName: string;
+  myLastName: string;
+  constructor() {
+     this.myName = "frank";
+    this.myLastName="";
   }
+  onSubmit() {
+    alert(this.myName);
   }
+}
