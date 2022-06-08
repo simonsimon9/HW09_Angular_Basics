c2cbe091b0e128786f3d3abc931225689c3028ed
simonsimon9 <simonkuang9@outlook.com>
2022-06-07 22:59:39 -0400

    using ngFor to iterate through array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,56 +5,31 @@ export interface PlayingCard{
   suit: string;
 }
 
-//Class is an object factory - adds extra js
-export class PlayingDud{
-  cardVal: string;
-  suit: string;
-
-  constructor(cardval: string, suit: string){
-    this.cardVal =  cardval;
-    this.suit = suit;
-  }
-
-  public get _suit(){
-    return this.suit;
-  }
-
-  public set _suit(suit:string){
-    this.suit = suit;
-  }
-
-  public get _cardVal(){
-    return this.cardVal;
-  }
-
-  public set _cardVal(cardVal:string){
-    this.cardVal = cardVal;
-  }
-  
-}
-
 //A. Decorator
 @Component({
   selector: 'app-root', //C. Service
-  template: `<h1>Hello world! {{title}} <br/>
-              {{card.cardVal}} of {{card.suit}} <br/>
-              {{card1.cardVal}} of {{card1.suit}}</h1><br/>
-              Card: <input [(ngModel)]="card.cardVal">
-              Suit: <input [(ngModel)]="card.suit">
-              `,
+  template: `<h1>Hello world! {{title}}</h1> <br/>
+  <ul *ngFor="let item of firstName; let i = index">
+    <li>{{"Item"+i+":"+item}}</li>
+</ul>
+<!-- Show cards in unordered list. -->
+<table>
+<tr *ngFor="let card of cards">
+<td> <ul> <li> {{card.cardVal}} </li> </ul> </td>
+<td> <ul> <li> {{card.suit}} </li> </ul> </td>
+</tr>
+</table>`
+              
   
 })
 export class AppComponent {
   title = 'This is Angular!';
-  // Declare and initialize a PlayingCard object.
-  constructor(){
-  }
-  // Declare a PlayingCard object.
-  card: PlayingCard = {
-    cardVal: "Ace",
-    suit: "Spades"
-    };
-  
-  card1 : PlayingDud = new PlayingDud("Queen", "Diamonds");
-  
+  // Include card data in collection.
+cards:PlayingCard[] = [
+  { cardVal: "Ace", suit: "Spades" },
+  { cardVal: "Two", suit: "Clubs" },
+  { cardVal: "Six", suit: "Hearts"} ]
+
+    //keep track index
+    firstName = ['abe', 'amy', 'al'];
   }
