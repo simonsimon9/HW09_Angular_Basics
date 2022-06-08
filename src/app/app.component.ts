fd4d750af201e9519ee5ce86b4b0100f1cdbe92b
simonsimon9 <simonkuang9@outlook.com>
2022-06-07 23:28:21 -0400

    ngIf to display element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,28 +8,29 @@ export interface PlayingCard{
 //A. Decorator
 @Component({
   selector: 'app-root', //C. Service
-  template: `<input [(ngModel)]="myinput">
-  <input type='button' (click)=doSomething(myinput) >
-  {{myoutput}}
-  {{myinput}} <br/>
-  
-  <input [(ngModel)]="myinput1">
-<input type='button' (click)=doSomething1(myinput1)>
-{{myoutput1}}`
+  template: `<h1>Hello world! {{title}} <br/>
+  <!-- Show cards in unordered list. -->
+  <ul><li *ngFor="let card of cards" (click)="onSelect(card)">
+  {{card.cardVal}}</li>
+  </ul>
+  <div *ngIf="selectedCard">
+  <h2>{{selectedCard.cardVal}} **</h2>
+  <input [(ngModel)]="selectedCard.cardVal" placeholder="name"/>
+  </div>
+  `
   
 })
 export class AppComponent {
-  myinput = '';
-  myinput1 = '';
-  myoutput = '';
-  myoutput1='';
-  doSomething(someContent : any) {
-    alert(someContent);
-    this.myoutput = someContent;
-  }
-
-  doSomething1(someContent: any){
-    alert(someContent);
-    this.myoutput1 = someContent;
+  title = 'This is Angular!';
+  // Include card data in class.
+  cards:PlayingCard[] = [
+  { cardVal:"Ace", suit:"Spades"},
+  { cardVal:"Two", suit:"Clubs" },
+  { cardVal:"Six", suit:"Hearts"},
+  ];
+  selectedCard!: PlayingCard;
+  onSelect(card: PlayingCard) {
+     alert(card.suit);
+     this.selectedCard = card;
   }
   }
